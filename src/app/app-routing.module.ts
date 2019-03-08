62a2785c798bde73402806d4aecc023cbfd8e619
Marc <1440411korver@zuyd.nl>
2019-03-08 20:19:48 +0100

    added FirebaseUI-Angular authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,9 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'tos', loadChildren: './pages/tos/tos.module#TosPageModule' },
+  { path: 'privacy', loadChildren: './pages/privacy/privacy.module#PrivacyPageModule' }
 ];
 
 @NgModule({
